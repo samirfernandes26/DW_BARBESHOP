1602e87a2d47cfa027fe5120b0dd750472577a98
Samir Fernandes de Lima Resende <samirfernandes@gmail.com>
2023-11-14 08:29:52 -0300

    provedor

--- lib/src/core/providers/application_providers.dart
+++ lib/src/core/providers/application_providers.dart
@@ -1,4 +1,6 @@
+import 'package:dw_barbershop/src/core/fp/either.dart';
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:dw_barbershop/src/model/user_model.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository_imp.dart';
 import 'package:dw_barbershop/src/services/users_login/user_logins_service.dart';
@@ -15,3 +17,12 @@ UserRepository userRepository(UserRepositoryRef ref) => UserRepositoryImpl(restC
 
 @Riverpod(keepAlive: true)
 UserLoginsService userLoginsService(UserLoginsServiceRef ref) => UserLoginsServiceImpl(userRepository: ref.read(userRepositoryProvider));
+
+@Riverpod(keepAlive: true)
+Future<UserModel> getMe(GetMeRef ref) async{
+  final result = await ref.watch(userRepositoryProvider).me();
+  return switch(result){
+    Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception,
+  };
+} 
